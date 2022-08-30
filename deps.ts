2724412f4cabcf715a6cff27297c5d3766f2b1ff
stefangohlke <gohlke.stefan@yahoo.de>
2022-08-30 23:36:50 +0200

    Revert Deno.serve change

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export {
    PersonService,
 } from 'https://raw.githubusercontent.com/sgohlke/ModPersonServiceDeno/1.0.13/personservice.ts';
+export { serve } from 'https://deno.land/std@0.153.0/http/server.ts';
--- webserver.ts
+++ webserver.ts
@@ -1,51 +1,39 @@
-import { PersonService } from './deps.ts';
+import { PersonService, serve } from "./deps.ts";
 
 const port = 3018;
 
 const jsonContentTypeHeader = {
-   'content-type': 'application/json; charset=UTF-8',
-};
+  "content-type": "application/json; charset=UTF-8",
+  };
 
-function logAndReturnErrorResponse(
-   errorMessage: string,
-   errorStatusCode = 400,
-): Response {
-   console.error(errorMessage);
-   return new Response(JSON.stringify({ error: errorMessage }), {
-      headers: jsonContentTypeHeader,
-      status: errorStatusCode,
-   });
+function logAndReturnErrorResponse(errorMessage: string, errorStatusCode = 400): Response {
+  console.error(errorMessage);
+  return new Response(JSON.stringify({error: errorMessage}), {
+    headers: jsonContentTypeHeader,
+    status: errorStatusCode
+  })
 }
 
 function handleRequest(request: Request): Response {
-   if (request.method !== 'GET') {
-      return logAndReturnErrorResponse(
-         `Only GET method is allowed, but got: ${request.method}`,
-         405,
-      );
-   }
-
-   const { pathname } = new URL(request.url);
-   if (pathname.includes('/person')) {
-      if (pathname === '/person' || pathname === '/person/') {
-         return new Response(JSON.stringify(PersonService.getAllPersons()), {
-            headers: jsonContentTypeHeader,
-         });
-      } else if (pathname.includes('/person/')) {
-         const userId = pathname.substring(8);
-         const person = PersonService.getPersonForId(userId);
-         if (!person) {
-            return logAndReturnErrorResponse(`No user found for id: ${userId}`);
-         } else {
-            return new Response(JSON.stringify(person), {
-               headers: jsonContentTypeHeader,
-            });
-         }
+  if (request.method !== "GET") {
+    return logAndReturnErrorResponse(`Only GET method is allowed, but got: ${request.method}`, 405)
+  }
+  
+  const { pathname } = new URL(request.url);
+  if (pathname.includes("/person")) {
+    if (pathname === "/person" || pathname === "/person/") {
+      return new Response(JSON.stringify(PersonService.getAllPersons()), {headers: jsonContentTypeHeader,})
+    } else if (pathname.includes("/person/") ) {
+      const userId = pathname.substring(8);
+      const person = PersonService.getPersonForId(userId);
+      if (!person) {
+        return logAndReturnErrorResponse(`No user found for id: ${userId}`);
+      } else {
+        return new Response(JSON.stringify(person), {headers: jsonContentTypeHeader},)
       }
-   }
-   return logAndReturnErrorResponse(
-      `No api endpoint found for path ${pathname}`,
-   );
+    } 
+  } 
+  return logAndReturnErrorResponse(`No api endpoint found for path ${pathname}`);
 }
 
-Deno.serve(handleRequest, { port: port });
+serve(handleRequest, { port: port });
