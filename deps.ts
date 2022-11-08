ff2c8daca882a4fdf4acd0e8d41bfc424fdc41b0
stefangohlke <gohlke.stefan@yahoo.de>
2022-11-08 23:32:34 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export {
    PersonService,
 } from 'https://raw.githubusercontent.com/sgohlke/ModPersonServiceDeno/1.0.13/personservice.ts';
-export { serve } from 'https://deno.land/std@0.153.0/http/server.ts';
+export { serve } from 'https://deno.land/std@0.163.0/http/server.ts';
